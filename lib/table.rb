ddcfd91455aa021f21bc838f828eb09e91d98df9
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-12 16:28:00 +0100

    Create Row class
    WIP Represents the data stored in a table row.

--- lib/table.rb
+++ lib/table.rb
@@ -12,7 +12,9 @@ class Table
       tds = tr.xpath('./td')
       constituency = tds.shift.text.strip.gsub("\n", ' — ') if tds.count > 2
       constituency_id = tds.shift.text if tds.count > 2
-      Row.new(tds).to_h.merge(constituency: constituency, constituency_id: constituency_id)
+      Row.new(tds)
+         .to_h
+         .merge(constituency: constituency, constituency_id: constituency_id)
     end
   end
 
