2b9b444de199eaf4acb68a95054699fa99768013
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-12 17:02:07 +0100

    Skip row when it does not contain member info
    The tables have a single column row at the top which does not contain member information. We skip these rows in the map loop -- the item becomes nil. We then use compact to remove the nil items.

--- lib/table.rb
+++ lib/table.rb
@@ -10,12 +10,11 @@ class Table
     constituency_id = nil
     table.xpath('.//tr[td]').map do |tr|
       tds = tr.xpath('./td')
+      next if tds.count == 1
       constituency = tds.shift.text.strip.gsub("\n", ' — ') if tds.count > 2
       constituency_id = tds.shift.text if tds.count > 2
-      Row.new(tds)
-         .to_h
-         .merge(constituency: constituency, constituency_id: constituency_id)
-    end
+      Row.new(tds).to_h.merge(constituency: constituency, constituency_id: constituency_id)
+    end.compact
   end
 
   private
