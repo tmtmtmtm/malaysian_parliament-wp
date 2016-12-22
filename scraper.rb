310d829a6bc5bffa4b53fa48194bb02246ea945d
Tony Bowden <tony@tmtm.com>
2016-12-22 18:34:11 +0000

    Rewrite scraper

--- scraper.rb
+++ scraper.rb
@@ -2,12 +2,17 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require 'scraperwiki'
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
+require 'scraped'
+require 'scraperwiki'
 require 'uri'
 
-require 'pry'
+require_rel 'lib/remove_notes'
+require_rel 'lib/remove_party_counts'
+require_rel 'lib/unspan_all_tables'
+
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
@@ -31,56 +36,121 @@ def noko_for(url)
   Nokogiri::HTML(open(url).read)
 end
 
-def wikilink(a)
-  return if a.attr('class') == 'new'
-  URI.join('http://en.wikipedia.org/', a['href']).to_s
+class MembershipRow < Scraped::HTML
+  field :id do
+    to_slugify = member ? member.attr('title') : tds[2].text
+    binding.pry if to_slugify.downcase.include? 'party'
+    to_slugify.downcase.tr(' ', '_').gsub('_(page_does_not_exist)', '')
+  end
+
+  field :name do
+    member ? member.text.strip : tds[2].text.strip
+  end
+
+  field :state do
+    noko.xpath('.//preceding::h3[1]').css('span.mw-headline').text.strip
+  end
+
+  field :constituency do
+    tds[1].text.strip
+  end
+
+  field :constituency_id do
+    '%s-%s' % [tds[0].text.strip, term]
+  end
+
+  field :wikipedia do
+    href = tds[2].xpath('.//a[not(@class="new")]/@href') or return
+    # TODO make this absolute again. This version is just to make sure
+    # we have minimal diffs
+    href.text.sub('https','http')
+  end
+
+  field :wikipedia__en do
+    href = tds[2].xpath('.//a[not(@class="new")]/@title') or return
+    href.text
+  end
+
+  field :party_id do
+    pid = party_and_coalition.first[:id]
+    return 'PKR' if pid == 'KeADILan'
+    pid
+  end
+
+  field :party do
+    party_and_coalition.first[:name]
+  end
+
+  field :term do
+    url.sub('https://en.wikipedia.org/wiki/Members_of_the_Dewan_Rakyat,_','').to_i
+  end
+
+  field :source do
+    url
+  end
+
+  field :area do
+    [constituency, state].reject(&:empty?).compact.join(', ')
+  end
+
+  field :coalition do
+    coalition_data[:name] if coalition_data
+  end
+
+  field :coalition_id do
+    coalition_data[:id] if coalition_data
+  end
+
+  def vacant?
+    tds[3].text == 'VAC' rescue binding.pry
+  end
+
+  private
+
+  def tds
+    @tds ||= noko.css('td')
+  end
+
+  def member
+    @member ||= tds[2].at_xpath('a')
+  end
+
+  def coalition_data
+    party_and_coalition.last
+  end
+
+  def party_and_coalition
+    unknown = { id: 'unknown', name: 'unknown' }
+    binding.pry unless td = tds[3]
+    return [] if tds[3].text == 'VAC'
+    #return [unknown, unknown] if td.css('a').count == 0
+    binding.pry if td.css('a').count == 0
+    expand = ->(a) { { id: a.text, name: a.xpath('@title').text.split('(').first.strip } }
+    return [expand.call(td.css('a')), nil] if td.css('a').count == 1
+    td.css('a').reverse.map { |a| expand.call(a) }
+  end
 end
 
-def wikiname(a)
-  return if a.attr('class') == 'new'
-  a.attr('title')
-end
+class ListPage < Scraped::HTML
+  decorator RemovePartyCounts
+  decorator UnspanAllTables
+  decorator RemoveNotes
+  decorator Scraped::Response::Decorator::AbsoluteUrls
 
-def party_and_coalition(td)
-  unknown = { id: 'unknown', name: 'unknown' }
-  return [unknown, unknown] unless td
-  expand = ->(a) { { id: a.text, name: a.xpath('@title').text.split('(').first.strip } }
-  return [expand.call(td.css('a')), nil] if td.css('a').count == 1
-  td.css('a').reverse.map { |a| expand.call(a) }
+  field :members do
+    noko.xpath('//table[.//th[.="Member"]]//tr[td[4]]').reject { |tr| tr.css('td').first.text == tr.css('td').last.text }.map do |row|
+      fragment row => MembershipRow
+    end
+  end
 end
-
 
 def scrape_term(term, url)
-  noko = noko_for(url)
-  added = 0
-  noko.xpath('//table[.//th[.="Member"]]//tr[td[2]]').each do |row|
-    tds = row.css('td')
-
-    member = tds[2].at_xpath('a') rescue nil
-    next unless member
-    (party, coalition) = party_and_coalition(tds[3])
-    data = {
-      id:              member.attr('title').downcase.tr(' ', '_').gsub('_(page_does_not_exist)', ''),
-      name:            member.text.strip,
-      state:           row.xpath('.//preceding::h3[1]').css('span.mw-headline').text.strip,
-      constituency:    tds[1].text.strip,
-      constituency_id: '%s-%s' % [tds[0].text.strip, term],
-      wikipedia:       wikilink(member),
-      wikipedia__en:   wikiname(member),
-      party_id:        party[:id],
-      party:           party[:name],
-      term:            term,
-      source:          url,
-    }
-    data[:area] = [data[:constituency], data[:state]].reject(&:empty?).compact.join(', ')
-    data[:party_id] = 'PKR' if data[:party_id] == 'KeADILan'
-    data[:coalition] = coalition[:name] if coalition
-    data[:coalition_id] = coalition[:id] if coalition
-    added += 1
-    puts data
-    ScraperWiki.save_sqlite(%i(id constituency term), data)
-  end
-  added
+  page = ListPage.new(response: Scraped::Request.new(url: url).response)
+  # TODO: can remove the reject once everything is consistent
+  data = page.members.reject { |m| m.vacant? }.map { |m| m.to_h.reject { |_,v| v.to_s.empty? } }
+  # puts data
+  ScraperWiki.save_sqlite(%i(id constituency term), data)
+  data.count
 end
 
 # Start with a clean slate…
