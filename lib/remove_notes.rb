50ebeddb6d1291da24eb99f7659e074be032d02f
Tony Bowden <tony@tmtm.com>
2017-01-04 15:54:31 +0000

    Add the lib files needed for the scraper
    These were omitted within the PR

---
+++ lib/remove_notes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require 'scraped'
+
+class RemoveNotes < Scraped::Response::Decorator
+  def body
+    Nokogiri::HTML(super).tap do |doc|
+      doc.css('sup.reference').remove
+    end.to_s
+  end
+end
---
+++ lib/remove_party_counts.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require 'scraped'
+require 'table_unspanner'
+
+# Remove all TRs that span the whole table — i.e with party counts
+class RemovePartyCounts < Scraped::Response::Decorator
+  def body
+    Nokogiri::HTML(super).tap do |doc|
+      doc.xpath('.//tr[.//td[@colspan="4"]]').remove
+    end.to_s
+  end
+end
